29e1878902e362a92c4d163bb41e5c1f73e6647a
Dan Kuenkas <dankuenkas@MacBook-Pro-sl-Dan.local>
2026-06-02 22:23:06 +0300

    update about team members

--- about-service/routes/aboutRoutes.js
+++ about-service/routes/aboutRoutes.js
@@ -2,12 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/about', (req, res) => {
-  res.json([
-    {
-      first_name: 'Dan',
-      last_name: 'Kuenkas'
-    }
-  ]);
+  try {
+    res.json([
+      {
+        first_name: 'Dan',
+        last_name: 'Kuenkas'
+      },
+      {
+        first_name: 'Nadav',
+        last_name: 'Golche Faragian'
+      }
+    ]);
+  } catch (error) {
+    res.status(500).json({
+      id: 'GET_ABOUT_ERROR',
+      message: error.message
+    });
+  }
 });
 
 module.exports = router;
